788adf2b401b901a4f98f3b1d911f0d1071b55cd
BharathS24 <BS00493532@techmahindra.com>
2018-03-20 13:09:06 +0530

    Added snmpmapper in Mapper
    Commiting new Module in Mapper
    Change-Id:I6fe49f5bf980bfc68abca89a7494f640942fe474
    Issue-ID: DCAEGEN2-338
    Signed-off-by: BharathS24 <BS00493532@techmahindra.com>

---
+++ snmpmapper/src/test/java/org/onap/dcae/mapper/FileUploadControllerTest.java
@@ -0,0 +1,16 @@
+package org.onap.dcae.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.onap.dcae.mapper.storage.StorageService;
+
+public class FileUploadControllerTest {
+	private FileUploadController fuc = new FileUploadController(storageService);
+
+	@Test
+	public void test() {
+		
+	}
+
+}
---
+++ snmpmapper/src/test/java/org/onap/dcae/mapper/SnmpmapperApplicationTest.java
@@ -0,0 +1,18 @@
+
+
+package org.onap.dcae.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnmpmapperApplicationTest {
+	
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
