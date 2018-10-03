dafe9d7d4146afa88c7ce89a550c82b4357a4f5b
amshegokar <AS00500801@techmahindra.com>
2018-10-03 19:49:00 +0530

    Spotify plugin changed same as inventory-api
     Docker spotify plugin changed as per inventory-api pom
    
    Change-Id: Ia99a3c0d182a6b8135692c6d3ddf5cd9d0dbe3e1
    Issue-ID: DCAEGEN2-335
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

--- UniversalVesAdapter/src/main/scripts/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "./conf:./lib/*" org.onap.universalvesadapter.Application
--- snmpmapper/src/main/scripts/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "./conf:./lib/*" org.onap.dcaegen2.services.mapper.snmpmapper.SnmpmapperApplication
