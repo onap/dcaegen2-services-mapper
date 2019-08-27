fa5d1e2af268c0c60a39faa5b41a40d165f6767c
aleemraja <ar00500721@techmahindra.com>
2019-08-27 18:02:47 +0530

    Consul update (via SDK library) to support periodic polling
    Issue-ID: DCAEGEN2-1735
    Change-Id: I0aeae0569aa94cec336688e1e70e1498a29473b4
    Signed-off-by: aleemraja <ar00500721@techmahindra.com>

--- UniversalVesAdapter/src/main/java/org/onap/universalvesadapter/utils/FetchDynamicConfig.java
+++ UniversalVesAdapter/src/main/java/org/onap/universalvesadapter/utils/FetchDynamicConfig.java
@@ -28,11 +28,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.util.Map;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.api.CbsRequests;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.model.CbsRequest;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.model.EnvProperties;
+import org.onap.dcaegen2.services.sdk.rest.services.model.logging.RequestDiagnosticContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -43,177 +51,182 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class FetchDynamicConfig {
-    
-    private static final Logger debugLogger = LoggerFactory.getLogger("debugLogger");
-    private static final Logger errorLogger = LoggerFactory.getLogger("errorLogger");
-    
-    private static String url;
-    public static String retString;
-    public static String retCBSString;
-    private static Map<String, String> env;
-    
-    public FetchDynamicConfig() {}
-    
-    public static void cbsCall(String configFile) {
-        
-        env = System.getenv();
-        Boolean areEqual;
-        // Call consul api and identify the CBS Service address and port
-        getconsul();
-        // Construct and invoke CBS API to get application Configuration
-        getCBS();
-        // Verify if data has changed
-        areEqual = verifyConfigChange(configFile);
-        
-        if (!areEqual) {
-            FetchDynamicConfig fc = new FetchDynamicConfig();
-            fc.writefile(retCBSString, configFile);
-        } else {
-            debugLogger
-                    .info("New config pull results identical -  " + configFile + " NOT refreshed");
-        }
-    }
-    
-    private static void getconsul() {
-        url = env.get("CONSUL_HOST") + ":8500/v1/catalog/service/"
-                + env.get("CONFIG_BINDING_SERVICE");
-        retString = executecurl(url);
-        debugLogger.info("CBS details fetched from Consul");
-    }
-    
-    public static boolean verifyConfigChange(String configFile) {
-        
-        boolean areEqual = false;
-        // Read current data
-        try {
-            
-            File f = new File(ClassLoader.getSystemResource(configFile.trim()).getFile());
-            
-            if (f.exists() && !f.isDirectory()) {
-                debugLogger.info(
-                        "Comparing local configuration with the configuration fethed from CBS ");
-                
-                String jsonData = readFile(configFile);
-                JSONObject jsonObject = new JSONObject(jsonData);
-                
-                ObjectMapper mapper = new ObjectMapper();
-                
-                JsonNode tree1 = mapper.readTree(jsonObject.toString());
-                JsonNode tree2 = mapper.readTree(retCBSString);
-                areEqual = tree1.equals(tree2);
-                debugLogger.info("Comparison value:" + areEqual);
-            } else {
-                debugLogger.info("First time config file read: " + configFile);
-            }
-            
-        } catch (IOException e) {
-            errorLogger.error("Comparison with new fetched data failed" + e.getMessage());
-            
-        }
-        
-        return areEqual;
-        
-    }
-    
-    public static void getCBS() {
-        
-        // consul return as array
-        JSONTokener temp = new JSONTokener(retString);
-        JSONObject cbsjobj = (JSONObject) new JSONArray(temp).get(0);
-        
-        String urlPart1 = null;
-        if (cbsjobj.has("ServiceAddress") && cbsjobj.has("ServicePort")) {
-            
-            urlPart1 = cbsjobj.getString("ServiceAddress") + ":" + cbsjobj.getInt("ServicePort");
-            
-        }
-        debugLogger.info("CONFIG_BINDING_SERVICE HOST:PORT is " + urlPart1);
-        
-        if (env.containsKey("HOSTNAME")) {
-            url = urlPart1 + "/service_component/" + env.get("HOSTNAME");
-            retCBSString = executecurl(url);
-            debugLogger.info("Configuration fetched from CBS successfully..");
-        } else if (env.containsKey("SERVICE_NAME")) {
-            url = urlPart1 + "/service_component/" + env.get("SERVICE_NAME");
-            retCBSString = executecurl(url);
-            debugLogger.info("Configuration fetched from CBS successfully..");
-        } else {
-            errorLogger.error(
-                    "Service name environment variable - HOSTNAME/SERVICE_NAME not found within container ");
-        }
-        
-    }
-    
-    public void writefile(String retCBSString, String configFile) {
-        
-        String indentedretstring = (new JSONObject(retCBSString)).toString(4);
-        File f = new File(ClassLoader.getSystemResource(configFile.trim()).getFile());
-        try {
-            debugLogger.info("Overwriting local configuration file " + configFile
-                    + " with configuartions received from CBS");
-            
-            
-            File file2 = ResourceUtils.getFile("classpath:" + configFile);
-            FileWriter fstream = new FileWriter(file2, false);
-            PrintWriter printWriter = new PrintWriter(fstream);
-            printWriter.print(indentedretstring);
-            printWriter.close();
-            fstream.close();
-            
-            debugLogger.info("New Config successfully written to local file to " + configFile);
-        } catch (IOException e) {
-            errorLogger.error("Error in writing configuration into local KV file " + configFile
-                    + retString + e.getMessage());
-            e.printStackTrace();
-        }
-        
-    }
-    
-    public static String readFile(String configFileName) {
-        String content = null;
-        File file = null;
-        
-        try {
-            file = ResourceUtils.getFile("classpath:" + configFileName);
-            content = new String(Files.readAllBytes(file.toPath()));
-        } catch (FileNotFoundException e) {
-            errorLogger.error("colud not find file :",configFileName);
-            
-        } catch (IOException e) {
-            errorLogger.error("unable to read the file , reason:", e.getCause());
-        } catch (Exception e) {
-            errorLogger.error("Exception occured , reason:", e.getMessage());
-        }
-        
-        return content;
-        
-    }
-    
-    private static String executecurl(String url) {
-        
-        String[] command = {"curl", "-v", url};
-        ProcessBuilder process = new ProcessBuilder(command);
-        Process p;
-        String result = null;
-        try {
-            p = process.start();
-            InputStreamReader ipr = new InputStreamReader(p.getInputStream());
-            BufferedReader reader = new BufferedReader(ipr);
-            StringBuilder builder = new StringBuilder();
-            String line;
-            
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-            result = builder.toString();
-            reader.close();
-            ipr.close();
-        } catch (IOException e) {
-            errorLogger.error("error", e);
-            e.printStackTrace();
-        }
-        return result;
-        
-    }
-    
+
+	// @Value("${defaultProtocol}")
+	static String defaultProtocol = "http";
+
+	private static final Logger debugLogger = LoggerFactory.getLogger("debugLogger");
+	private static final Logger errorLogger = LoggerFactory.getLogger("errorLogger");
+
+	private static String url;
+	public static String retString;
+	public static String retCBSString;
+
+	// Generate RequestID and InvocationID which will be used when logging and in
+	// HTTP requests
+	final RequestDiagnosticContext diagnosticContext = RequestDiagnosticContext.create();
+	final CbsRequest request = CbsRequests.getConfiguration(diagnosticContext);
+
+	// Read necessary properties from the environment
+	static final EnvProperties env = EnvProperties.fromEnvironment();
+
+	public FetchDynamicConfig() {
+
+	}
+
+	public static void cbsCall(String configFile) {
+
+		Boolean areEqual;
+		// Call consul api and identify the CBS Service address and port
+		getconsul();
+		// Construct and invoke CBS API to get application Configuration
+		getCBS();
+		// Verify if data has changed
+		areEqual = verifyConfigChange(configFile);
+
+		if (!areEqual) {
+			FetchDynamicConfig fc = new FetchDynamicConfig();
+			if (retCBSString!=null) {
+				fc.writefile(retCBSString, configFile);
+			} else {
+				debugLogger.debug("No content recieved from server");
+			}
+			
+		} else {
+			debugLogger.info("New config pull results identical -  " + configFile + " NOT refreshed");
+		}
+	}
+
+	private static void getconsul() {
+		url = defaultProtocol + "://" + env.consulHost() + ":" + env.consulPort() + "/v1/catalog/service/"
+				+ env.cbsName();
+		retString = fetchResultFromDestination(url);
+		debugLogger.info("CBS details fetched from Consul");
+	}
+
+	public static boolean verifyConfigChange(String configFile) {
+
+		boolean areEqual = false;
+		// Read current data
+		try {
+
+			File f = new File(ClassLoader.getSystemResource(configFile.trim()).getFile());
+
+			if (f.exists() && !f.isDirectory()) {
+				debugLogger.info("Comparing local configuration with the configuration fethed from CBS ");
+
+				String jsonData = readFile(configFile);
+				JSONObject jsonObject = new JSONObject(jsonData);
+
+				ObjectMapper mapper = new ObjectMapper();
+
+				JsonNode tree1 = mapper.readTree(jsonObject.toString());
+				JsonNode tree2 = mapper.readTree(retCBSString);
+				areEqual = tree1.equals(tree2);
+				debugLogger.info("Comparison value:" + areEqual);
+			} else {
+				debugLogger.info("First time config file read: " + configFile);
+			}
+
+		} catch (IOException e) {
+			errorLogger.error("Comparison with new fetched data failed" + e.getMessage());
+
+		}
+
+		return areEqual;
+
+	}
+
+	public static void getCBS() {
+
+		// consul return as array
+		JSONTokener temp = new JSONTokener(retString);
+		JSONObject cbsjobj = (JSONObject) new JSONArray(temp).get(0);
+
+		String urlPart1 = null;
+		if (cbsjobj.has("ServiceAddress") && cbsjobj.has("ServicePort")) {
+
+			urlPart1 = cbsjobj.getString("ServiceAddress") + ":" + cbsjobj.getInt("ServicePort");
+
+		}
+		debugLogger.info("CONFIG_BINDING_SERVICE HOST:PORT is " + urlPart1);
+
+		if (env.appName() != null) {
+			url = defaultProtocol + "://" + urlPart1 + "/service_component/" + env.appName();
+			retCBSString = fetchResultFromDestination(url);
+			debugLogger.info("Configuration fetched from CBS successfully..");
+		} else {
+			errorLogger.error("Service name environment variable - APP_NAME/SERVICE_NAME not found within container ");
+		}
+
+	}
+
+	public void writefile(String retCBSString, String configFile) {
+
+		String indentedretstring = (new JSONObject(retCBSString)).toString(4);
+		try {
+			debugLogger.info(
+					"Overwriting local configuration file " + configFile + " with configuartions received from CBS");
+
+			File file2 = ResourceUtils.getFile("classpath:" + configFile);
+			FileWriter fstream = new FileWriter(file2, false);
+			PrintWriter printWriter = new PrintWriter(fstream);
+			printWriter.print(indentedretstring);
+			printWriter.close();
+			fstream.close();
+
+			debugLogger.info("New Config successfully written to local file to " + configFile);
+		} catch (IOException e) {
+			errorLogger.error(
+					"Error in writing configuration into local KV file " + configFile + retString + e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String readFile(String configFileName) {
+		String content = null;
+		File file = null;
+
+		try {
+			file = ResourceUtils.getFile("classpath:" + configFileName);
+			content = new String(Files.readAllBytes(file.toPath()));
+		} catch (FileNotFoundException e) {
+			errorLogger.error("colud not find file :", configFileName);
+
+		} catch (IOException e) {
+			errorLogger.error("unable to read the file , reason:", e.getCause());
+		} catch (Exception e) {
+			errorLogger.error("Exception occured , reason:", e.getMessage());
+		}
+
+		return content;
+	}
+
+	private static String fetchResultFromDestination(String url) {
+		debugLogger.debug("FetchDynamicConfig : fetchResultFromDestination : START");
+
+		StringBuffer sb = new StringBuffer();
+		try {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet(url);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+				sb.append('\n');
+			}
+		} catch (ClientProtocolException e) {
+			debugLogger.debug("FetchDynamicConfig : fetchResultFromDestination : ClientProtocolException thrown."+e.getMessage());
+		} catch (UnsupportedOperationException e) {
+			debugLogger.debug("FetchDynamicConfig : fetchResultFromDestination : UnsupportedOperationException thrown."+e.getMessage());
+		} catch (IOException e) {
+			debugLogger.debug("FetchDynamicConfig : fetchResultFromDestination : IOException thrown."+e.getMessage());
+		}
+
+		debugLogger.debug("FetchDynamicConfig : fetchResultFromDestination : END");
+		return sb.toString();
+	}
+
 }
